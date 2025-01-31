6cd4f860f56b800f6c7800b20b5d85f2d707e0a5
Adrian Robinson <adrian.j.robinson@gmail.com>
2025-01-31 20:33:30 +0000

    feat: Update and rename example.sh to hda_audio.sh

--- files/scripts/hda_audio.sh
+++ files/scripts/hda_audio.sh
@@ -5,6 +5,4 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-# Your code goes here.
-echo 'This is an example shell script'
-echo 'Scripts here will run during build if specified in recipe.yml'
+echo '=== hda_audio.sh ==='
