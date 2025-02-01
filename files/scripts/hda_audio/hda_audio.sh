94b3b9429ad010a793f8467a54366e449aea61ef
Adrian Robinson <adrian.j.robinson@gmail.com>
2025-02-01 10:22:35 +0000

    fix: Update and rename files/scripts/hda_audio.sh to files/scripts/hda_audio/hda_audio.sh

--- files/scripts/hda_audio/hda_audio.sh
+++ files/scripts/hda_audio/hda_audio.sh
@@ -51,6 +51,9 @@ mv $hda_dir/patch_cirrus.c $hda_dir/patch_cirrus.c.orig
 echo " * copy patched files"
 cp $patch_dir/Makefile $patch_dir/patch_cirrus.c $patch_dir/patch_cirrus_a1534_setup.h $patch_dir/patch_cirrus_a1534_pcm.h $hda_dir/
 
+echo " * altering Makefile"
+sed -i 's/depmod -a/depmod -a $(KERNELRELEASE)/' $hda_dir/Makefile
+
 # if kernel version is >= 6.12 then change
 # snd_pci_quirk to hda_quirk
 # SND_PCI_QUIRK to HDA_CODEC_QUIRK
@@ -67,6 +70,7 @@ update_dir="/lib/modules/$kernel_release/updates"
 export KERNELRELEASE=$kernel_release # this is needed for 'make'
 echo " * compiling kernel module"
 make
+make install
 
 echo " * copying kernel module into $update_dir"
 cp $hda_dir/snd-hda-codec-cirrus.ko $update_dir # 'make install' did not work
