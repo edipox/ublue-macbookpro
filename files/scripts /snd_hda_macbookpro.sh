6dffcaa2a46032043bb68bb34843adb5b23a2d9a
Adrian Robinson <adrian.j.robinson@gmail.com>
2025-02-01 18:55:13 +0000

    fix: Update and rename files/scripts/hda_audio/install.sh to files/scripts /snd_hda_macbookpro.sh

--- files/scripts /snd_hda_macbookpro.sh
+++ files/scripts /snd_hda_macbookpro.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+# Script to install sound patches from: https://github.com/davidjo/snd_hda_macbookpro
 
 # fail if any errors
 set -oue pipefail
 
-# 'uname -r' returns 'azure', use this instead:
-kernel_release=$(rpm -qa kernel | cut -d '-' -f2-) # cut from the second field by '-' onwards => 6.11.8-300.fc41.x86_64
+# get the kernel release version string to pass to the script
+# as 'uname -r' returns 'azure' in Github build environment
+# we need to get the installed kernel from rpm => kernel-6.11.8-300.fc41.x86_64
+# now cut from the second field by '-' onwards => 6.11.8-300.fc41.x86_64
+kernel_release=$(rpm -qa kernel | cut -d '-' -f2-) 
 echo "Using kernel release: $kernel_release ..."
 
-# make a directory as the install script fails without this in place
+# make a target directory
 echo "Creating directory /lib/modules/$kernel_release/updates ..."
 mkdir -p /lib/modules/$kernel_release/updates
 
@@ -16,6 +20,7 @@ git clone https://github.com/davidjo/snd_hda_macbookpro /tmp/snd_hda_macbookpro
 cd /tmp/snd_hda_macbookpro
 
 # edit the Makefile to explicity state the kernel release
+# otherwise this uses the 'uname -r' environment which will fail
 echo "Edit the Makefile to add $kernel_release ..."
 sed -i "s/depmod -a/depmod -a $kernel_release/" Makefile
 
