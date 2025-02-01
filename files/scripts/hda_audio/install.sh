8c0769b0f6d43bec8684d05536fef9178abb4bdf
Adrian Robinson <adrian.j.robinson@gmail.com>
2025-02-01 17:41:42 +0000

    feat: Create install.sh

---
+++ files/scripts/hda_audio/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 'uname -r' returns 'azure', use this instead:
+kernel_release=$(rpm -qa kernel | cut -d '-' -f2-) # cut from the second field by '-' onwards => 6.11.8-300.fc41.x86_64
+echo "Using kernel release: $kernel_release ..."
+
+echo "Cloning and moving to the git repository ..."
+git clone https://github.com/davidjo/snd_hda_macbookpro /tmp/snd_hda_macbookpro && cd /tmp/snd_hda_macbookpro
+
+# edit the Makefile to explicity state the kernel release
+echo "Edit the Makefile to add $kernel_release ..."
+sed -i '/depmod -a/depmod -a $kernel_release/' Makefile
+
+echo "Calling the install script ..."
+./install.cirrus.driver.sh -i -k $kernel_release
+
+echo "Cleaning up ..."
+rm -rf /tmp/snd_hda_macbookpro
