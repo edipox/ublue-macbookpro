0bc233c35b0eb19d2b9f8407183563de60e2eb79
Adrian Robinson <adrian.j.robinson@gmail.com>
2025-02-01 19:16:05 +0000

    feat: Create facetimehd.sh

---
+++ files/scripts /facetimehd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script to install facetimehd from akmods (the Bluebuild yml was not working)
+
+# fail if any errors
+set -oeux pipefail
+
+echo "Downloading build-kmod-facetimehd.sh script ..."
+curl -LsSf -o /tmp/build-kmod-facetimehd.sh 
+    "https://raw.githubusercontent.com/ublue-os/akmods/refs/heads/main/build_files/extra/build-kmod-facetimehd.sh"
+
+echo "Marking as executable ..."
+chmod +x /tmp/build-kmod-facetimehd.sh
+
+echo "Calling the install script ..."
+bash /tmp/build-knod-facetimehd.sh
+
+echo "Cleaning up ..."
+rm /tmp/build-kmod-facetimehd.sh
