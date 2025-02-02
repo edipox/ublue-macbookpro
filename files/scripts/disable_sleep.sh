8c709ab53ed794e8eef1b7081374829650f2b5df
Adrian Robinson <adrian.j.robinson@gmail.com>
2025-02-02 15:51:40 +0000

    fix: Update disable_sleep.sh

--- files/scripts/disable_sleep.sh
+++ files/scripts/disable_sleep.sh
@@ -7,3 +7,7 @@ set -oue pipefail
 
 echo "Disabling sleep / suspend / hibernate / hybrid-sleep ..."
 systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+# could try with enabling some of these, and having the following on first boot...
+# https://github.com/Dunedan/mbp-2016-linux?tab=readme-ov-file#suspend--hibernation
+# echo 0 > /sys/bus/pci/devices/0000\:01\:00.0/d3cold_allowed
