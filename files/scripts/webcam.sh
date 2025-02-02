42395277673bc9ed42ee684e55256ac8be721020
Adrian Robinson <adrian.j.robinson@gmail.com>
2025-02-02 08:34:37 +0000

    fix: Update webcam.sh

--- files/scripts/webcam.sh
+++ files/scripts/webcam.sh
@@ -7,28 +7,19 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME:-kernel}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
-if [[ "${RELEASE}" -ge 41 ]]; then
-    COPR_RELEASE="rawhide"
-else
-    COPR_RELEASE="${RELEASE}"
-fi
-
 curl -L -o /etc/yum.repos.d/_copr_mulderje-intel-mac-rpms.repo \
-    "https://copr.fedorainfracloud.org/coprs/mulderje/intel-mac-rpms/repo/fedora-$(rpm -E %fedora)/mulderje-intel-mac-rpms-fedora-$(rpm -E %fedora).repo"
-#rpm-ostree install facetimehd-kmod
+    "https://copr.fedorainfracloud.org/coprs/mulderje/intel-mac-rpms/repo/fedora-$(RELEASE)/mulderje-intel-mac-rpms-fedora-$(RELEASE).repo"
 
-# original command
-#curl -LsSf -o /etc/yum.repos.d/_copr_mulderje-facetimehd-kmod.repo \
-#    "https://copr.fedorainfracloud.org/coprs/mulderje/facetimehd-kmod/repo/fedora-${COPR_RELEASE}/mulderje-facetimehd-kmod-fedora-${COPR_RELEASE}.repo"
+
+chmod a=rwx,u+t /tmp # fix /tmp permissions
+mkdir -p /run/akmods # fix missing location for lock file
 
 dnf5 install -y akmod-facetimehd-*.fc${RELEASE}.${ARCH}
-chmod a=rwx,u+t /tmp # fix /tmp permissions
-mkdir -p /run/akmods
-#chmod a=rwx,u+t /run/akmods # fix /run/akmods ? permissions lets see...
 
-akmods --force --kernels "${KERNEL}" #--kmod facetimehd
-
-cat /var/cache/akmods/facetimehd/0.6.8.1-1.20240319git0.6.8.1-for-6.12.9-200.fc41.x86_64.failed.log
+# fix the --gpgcheck error for akmods
+# see: https://universal-blue.discourse.group/t/need-help-building-system76-io-akmods/5725/3
+sed -i "s/--nogpgcheck//g"  /usr/sbin/akmods
+akmods --force --kernels "${KERNEL}" --kmod facetimehd
 
 #akmodsbuild --kernels "${KERNEL}" /usr/src/akmods/facetimehd-kmod-*.src.rpm
 
