ce63d50c7131386771c123c53b7e3101da868336
Adrian Robinson <adrian.j.robinson@gmail.com>
2025-02-02 15:54:54 +0000

    fix: Update disable_sleep.sh

--- files/scripts/disable_sleep.sh
+++ files/scripts/disable_sleep.sh
@@ -8,6 +8,27 @@ set -oue pipefail
 echo "Disabling sleep / suspend / hibernate / hybrid-sleep ..."
 systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 
-# could try with enabling some of these, and having the following on first boot...
+# could try with enabling some of these, and having the following on boot
+#
+# in /etc/rc.local:
+# #!/bin/sh
 # https://github.com/Dunedan/mbp-2016-linux?tab=readme-ov-file#suspend--hibernation
 # echo 0 > /sys/bus/pci/devices/0000\:01\:00.0/d3cold_allowed
+# exit 1
+#
+# in /etc/systemd/rc-local.service:
+# [Unit]
+# Description=/etc/rc.local
+# ConditionPathExists=/etc/rc.local
+# [Service]
+# ExecStart=/etc/rc.local start
+# TimeoutSec=0
+# StandardOutput=tty
+# RemainAfterExit=yes
+# SysVStartPriority=99
+# [Install]
+# WantedBy=multi-user.target
+#
+# then enable the service:
+# $ systemctl enable rc-local
+#
